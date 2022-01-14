d10f294c57e16293d65cfb291dc761209e933279
Maitreya29 <maitreyapatni30@gmail.com>
2022-01-14 10:22:14 +0530

    Settings: Add pitch black theme [2/2]

--- src/com/android/settings/homepage/PersonalizationsFragment.java
+++ src/com/android/settings/homepage/PersonalizationsFragment.java
@@ -17,18 +17,62 @@
 package com.android.settings.homepage;
 
 import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.UserHandle;
+
+import androidx.preference.Preference;
+import com.radiant.support.preferences.SwitchPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
-public class PersonalizationsFragment extends DashboardFragment {
+public class PersonalizationsFragment extends DashboardFragment implements OnPreferenceChangeListener {
 
     private static final String TAG = "PersonalizationsFragment";
+    private SwitchPreference mPitchPreference;
+    IOverlayManager mOverlayManager;
+
 
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.Radiant;
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        super.onCreatePreferences(savedInstanceState, rootKey);
+        mOverlayManager = IOverlayManager.Stub.asInterface(ServiceManager.getService("overlay"));
+        mPitchPreference = findPreference("pitch_theme");
+        try {
+            mPitchPreference.setChecked(mOverlayManager.getOverlayInfo("com.radiant.pitchsystem", UserHandle.USER_CURRENT).isEnabled());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        mPitchPreference.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPitchPreference) {
+            setOverlay("com.radiant.pitchsystem", (Boolean) newValue);
+            setOverlay("com.radiant.pitchsettings", (Boolean) newValue);
+            setOverlay("com.radiant.pitchsystemui", (Boolean) newValue);
+            return true;
+        }
+        return false;
+    }
+
+    private void setOverlay(String overlay, boolean status) {
+        try {
+            mOverlayManager.setEnabled(overlay, status, UserHandle.USER_CURRENT);
+        } catch (RemoteException | IllegalStateException | SecurityException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
